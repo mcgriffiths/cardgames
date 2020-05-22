9d8615843f239c19d00b7f94a446c94e63220175
Martin Griffiths <martin.griffiths@ofsted.gov.uk>
2020-05-22 16:03:12 +0100

    fixing colour error

--- fuji_template.R
+++ fuji_template.R
@@ -9,10 +9,10 @@ card_list <- tibble(rank = 2:20) %>%
 make_card <- function(df){
   
   ggplot(df) +
-    geom_rect(xmin = 0, xmax = 1, ymin = 0, ymax = 1, aes(fill = rank)) +
+    geom_rect(xmin = 0, xmax = 1, ymin = 0, ymax = 1, aes(fill = factor(rank))) +
     geom_text(x = 0.15, y = 0.9, hjust = 'center', size = 5, colour = 'black', aes(label = rank)) +
     geom_text(x = 0.5, y = 0.5, hjust = 'center', size = 20, colour = 'black', aes(label = rank)) +
-    scale_fill_gradientn(colours = rainbow(7)) +
+    scale_fill_manual(values = rainbow(20)) +
     scale_x_continuous(limits = c(0,1), expand = c(0,0)) +
     scale_y_continuous(limits = c(0,1), expand = c(0,0)) +
     facet_wrap(~ rank, ncol = 4) +
