7f12ef521ee8621f993f989de66f2b2d9bdb79c9
Martin Griffiths <martin.griffiths@gmail.com>
2020-05-25 20:30:03 +0100

    improved fuji template

--- fuji_template.R
+++ fuji_template.R
@@ -10,8 +10,9 @@ make_card <- function(df){
   
   ggplot(df) +
     geom_rect(xmin = 0, xmax = 1, ymin = 0, ymax = 1, aes(fill = factor(rank))) +
-    geom_text(x = 0.15, y = 0.9, hjust = 'center', size = 5, colour = 'black', aes(label = rank)) +
-    geom_text(x = 0.5, y = 0.5, hjust = 'center', size = 20, colour = 'black', aes(label = rank)) +
+    geom_label(x = 0.15, y = 0.9, hjust = 'center', size = 5, colour = 'black', aes(label = rank)) +
+    geom_label(x = 0.85, y = 0.1, hjust = 'center', angle = 180, size = 5, colour = 'black', aes(label = rank)) +
+    geom_label(x = 0.5, y = 0.5, hjust = 'center', size = 20, colour = 'black', aes(label = rank)) +
     scale_fill_manual(values = set_names(rainbow(19), 2:20)) +
     scale_x_continuous(limits = c(0,1), expand = c(0,0)) +
     scale_y_continuous(limits = c(0,1), expand = c(0,0)) +
